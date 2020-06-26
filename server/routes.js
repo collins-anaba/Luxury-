1dd9a63ca4cb1f23ec99110b211b10a9935ec4b1
collins anaba <evulukwu@me.com>
2020-06-25 20:31:16 -0500

    added a server

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
 
 
-
-
-router.post('/signup', (req,res) => {
-    console.log(req.body)
-    res.send('You made a post request')
+//endpoint post request
+router.post('/signup', async (req,res) => {
+    const {email, password} = req.body;
+    //try catch to catch duplicate emails
+    try{
+        const user = new User({email,password});
+        //saves post request to the db
+        await user.save();
+        res.send('You made a post request')
+    } catch(err){
+        return res.status(422).send(err.message)
+    }
 });
 
 module.exports = router;
