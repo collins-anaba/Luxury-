fd2b681956e38342c8bcfeb6ad539095b704cef1
collins anaba <evulukwu@me.com>
2020-06-26 10:14:54 -0500

    added signin and signup endpoints to server

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -10,7 +10,58 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    telephoneNumber: {
+        type: Number,
+        required: true
     }
 });
+
+// pre call hook that saves the instance of a user to the database. use the word function and not an anonymous function.
+userSchema.pre('save', function(){
+    const user = this;
+    // if password has not been changed then don't salt the password
+    if(!user.isModified('password')){
+        return next()
+    }
+    // 10 is how complex the sale algorithm is
+bcrypt.genSalt(10, (err, salt) => {
+if(err){
+    return next(err);
+}
+//hashes the password
+bcrypt.hash(user.password,salt, (err, hash) => {
+    if(err){
+        return next(err);
+    }
+    user.password = hash
+    next();
+});
+});
+});
+
+// compares password created by the user to password stored in the database
+userSchema.methods.comparePassword = function(candidatePassword){
+    const user = this;
+return new Promise((resolve, reject)=> {
+    bcrypt.compare(candidatePassword,user.password, (err, isMatch)=> {
+        if(err){
+            return reject(err)
+        }
+        if(!isMatch){
+            return reject(false)
+        };
+        resolve(true)
+    });
+})
+}
 
 mongoose.model('User', userSchema)
--- server/routes.js
+++ server/routes.js
@@ -7,10 +7,10 @@ const User = mongoose.model('User')
 
 //endpoint post request
 router.post('/signup', async (req,res) => {
-    const {email, password} = req.body;
+    const {email, password, firstName, lastName,telephoneNumber} = req.body;
     //try catch to catch duplicate emails
     try{
-        const user = new User({email,password});
+        const user = new User({email,password,firstName,lastName,telephoneNumber});
         //saves post request to the db
         await user.save();
         //creating a token
@@ -21,4 +21,21 @@ router.post('/signup', async (req,res) => {
     }
 });
 
+router.post('/signin', async (req,res)=> {
+    const {email, password} =req.body;
+    if(!email || !password){
+        return res.status(422).send({error: 'Must provide email and password'})
+    }
+   const user =  await User.findOne({email:email});
+   if (!user){
+       res.status(422).send({error: 'Invalid password or email'})
+   }
+   try {
+       await user.comparePassword(password);
+       const token = jwt.sign({userId:user._id},'SECRET_SESSION')
+        res.send({token:token})
+    } catch(err){
+        return res.status(422).send({error: 'Invalid password or email'})
+    }
+});
 module.exports = router;
